6f00da6cdd6ed24c462f860a5bb83bc4b01b29e7
Sabrina <Sabrina@Sabrinas-MacBook-Pro-4.local>
2018-03-04 19:42:51 -0500

    Refactoring, small UI upgrades
    MathProblem.cs - added comments, let user hit enter to submit answer, have input box stay in focus when an answer is submitted, move equation logic into its own class, start implementing levels.
    
    MathEquation.cs - holds all relevant information and functions for generating math equations.

---
+++ MathGame/Assets/Scripts/MathEquation.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	/// <summary>
+	/// holds math equation
+	/// </summary>
+	public class MathEquation
+	{
+		/// <summary>
+		/// Number used in addition equation
+		/// </summary>
+		private int num1, num2;
+
+		/// <summary>
+		/// Generates random numbers
+		/// </summary>
+		private System.Random rnd;
+
+		private bool isAddition, isSubtraction;
+		private int level, increaseRange;
+
+		public MathEquation (int level, int increaseRange, bool isAddition = false, bool isSubtraction = false)
+		{
+			this.isAddition = isAddition;
+			this.isSubtraction = isSubtraction;
+			this.level = level;
+			this.increaseRange = increaseRange;
+
+			// randomly generate 2 numbers between 0-10 for the math problem
+			rnd = new System.Random ();
+			num1 = rnd.Next (0, level*increaseRange);
+			num2 = rnd.Next (0, level*increaseRange);
+		}
+
+		/// <summary>
+		/// returns num1
+		/// </summary>
+		public int Num1
+		{
+			get { return num1; }
+		}
+
+		/// <summary>
+		/// returns num2
+		/// </summary>
+		public int Num2 
+		{
+			get { return num2; }
+		}
+
+		/// <summary>
+		/// returns the sum of num1 and num2
+		/// </summary>
+		public int Sum 
+		{
+			get { return num1 + num2; }
+		}
+
+		public int Level 
+		{
+			get { return level; }
+			set { 
+				if (value > 0)
+					level = value;
+				else
+					level = 1;
+			}
+		}
+
+		public void IncreaseLevel() 
+		{
+			level++;
+		}
+
+
+		/// <summary>
+		/// Gets new numbers for the math problem.
+		/// Makes sure exact equations aren't repeated.
+		/// num1 and num2 can exchange values and will still 
+		/// be considered a new equation.
+		/// </summary>
+		public void GenerateNewEquation()
+		{
+			// get new random numbers for the math problem
+			int tmpNum1 = rnd.Next (0, level*increaseRange);
+			int tmpNum2 = rnd.Next (0, level*increaseRange);
+
+			if (isAddition)
+			{
+				// make sure it is not the same problem as was just given
+				while (tmpNum1 == num1 && tmpNum2 == num2)
+				{
+					tmpNum1 = rnd.Next (0, level*increaseRange);
+					tmpNum2 = rnd.Next (0, level*increaseRange);
+				}
+			}
+			else if (isSubtraction)
+			{
+				// make sure it is not the same problem as was just given
+				// and make sure the answer won't be negative
+				while ((tmpNum1 == num1 && tmpNum2 == num2)
+					|| tmpNum1 < tmpNum2)
+				{
+					tmpNum1 = rnd.Next (0, level*increaseRange);
+					tmpNum2 = rnd.Next (0, level*increaseRange);
+				}
+			}
+
+			// set variables to reflect the new numbers
+			num1 = tmpNum1;
+			num2 = tmpNum2;
+		}
+	}
+}
+
--- MathGame/Assets/Scripts/MathProblem.cs
+++ MathGame/Assets/Scripts/MathProblem.cs
@@ -3,58 +3,122 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 using UnityEngine.UI;
+using AssemblyCSharp;
+
 
 public class MathProblem : MonoBehaviour {
 
+	#region Variable Declararion
 	public Text mathProblem, userInput;
-	private int num1, num2, sum;
-	private System.Random rnd;
 	public Button enterButton;
+	private MathEquation equation;
+	private string INPUT = "userInput";
+	private GameObject inputFieldGO;
+	private InputField InputFieldCO;
+	private bool isFocused;
+	private int correctAnswers;
+	private int level;
+	private int increaseRange;
+	#endregion
 
-	// Use this for initialization
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
 	void Start ()
 	{
 		// add a listener for when user clicks enter button
 		enterButton
 			.onClick
-			.AddListener (TaskOnClick);
+			.AddListener (CheckAnswer);
 
-		// randomly generate 2 numbers between 0-10 for the math problem
-		rnd = new System.Random ();
-		num1 = rnd.Next (0, 10);
-		num2 = rnd.Next (0, 10);
-		sum = num1 + num2;
-		mathProblem.text = num1 + " + " + num2 + " = ";
+		// get the input field as a game object and an input field object
+		inputFieldGO = GameObject.Find (INPUT);
+		InputFieldCO = inputFieldGO.GetComponent<InputField> ();
+
+		isFocused = false;
+		correctAnswers = 0;
+
+		level = 1;
+		increaseRange = 10;
+
+		equation = new MathEquation (level: level, increaseRange: increaseRange, isAddition: true);
+		mathProblem.text = equation.Num1 + " + " + equation.Num2 + " = ";
 	}
-	
-	// Update is called once per frame
+
+	/// <summary>
+	/// Update is called once per frame, checks user input
+	/// </summary>
 	void Update () 
 	{
-		GameObject inputFieldGO = GameObject.Find ("userInput");
-		InputField InputFieldCO = inputFieldGO.GetComponent<InputField> ();
-		userInput.text = InputFieldCO.text;
-	}
 
-	void TaskOnClick() 
-	{
-		int input;
+		inputFieldGO = GameObject.Find (INPUT);
 
-		GameObject inputFieldGO = GameObject.Find ("userInput");
-		InputField InputFieldCO = inputFieldGO.GetComponent<InputField> ();
-		int.TryParse(InputFieldCO.text, out input);
-
-		if (input == sum) 
+		// if the user presses enter, take that as if they clicked the enter button
+		// check if the answer is correct
+		if ((Input.GetKeyDown ("enter") || Input.GetKeyDown ("return")) && isFocused)
 		{
-			Debug.Log ("success");
-			num1 = rnd.Next (0, 10);
-			num2 = rnd.Next (0, 10);
-			sum = num1 + num2;
-			mathProblem.text = num1 + " + " + num2 + " = ";
+			CheckAnswer ();
 		} 
 		else 
 		{
-			Debug.Log ("failure");
+			// update the number being displayed in the input box to reflect
+			// what the user has entered into it
+			userInput.text = InputFieldCO.text;
 		}
+
+		// keep tabs of if input box is in focus or not
+		// needed to register if input box is in focus and user clicks enter
+		// because when the user hits enter it immediately goes out of focus
+		if (InputFieldCO.isFocused)
+		{
+			isFocused = true;
+		}
+		else
+		{
+			isFocused = false;
+		}
+	}
+
+	/// <summary>
+	/// Checks the answer to the math problem given by the user.
+	/// If correct, a new math problem is generated.
+	/// </summary>
+	void CheckAnswer() 
+	{
+
+		// get the number entered by the user.
+		// don't really need to validate it's a number because the input
+		// field only allows integers
+		int input;
+		int.TryParse(InputFieldCO.text, out input);
+
+		if (input == equation.Sum) 
+		{ // user answered correctly
+			Debug.Log ("success");
+
+			// generate a new math problem & update display
+			correctAnswers++;
+			equation.GenerateNewEquation ();
+			mathProblem.text = equation.Num1 + " + " + equation.Num2 + " = ";
+			InputFieldCO.text = string.Empty;
+			InputFieldCO.ActivateInputField();
+
+			if (correctAnswers == 10)
+			{
+				IncreaseDifficulty ();
+			}
+
+		} 
+		else 
+		{ // user answered incorrectly
+			Debug.Log ("failure");
+			InputFieldCO.ActivateInputField();
+		}
+
+	}
+
+	private void IncreaseDifficulty() 
+	{
 
 	}
 }
