ae7708306194f0a1b2144c73cc7c3cf26b08e2e5
Sabrina <Sabrina@Sabrinas-MacBook-Pro-4.local>
2018-04-08 18:01:07 -0400

    Create script for subtraction game
    - Rename Space scene to Addition
    
    - Rename MathProblem.cs to AdditionProblem.cs
    
    PlayAnimation.cs - let user pass in state name for animations, create an animation function that waits for the animation to finish
    
    - create Subtraction scene
    
    SubtractionProblem.cs - create code to run game for subtraction

--- MathGame/Assets/Scripts/PlayAnimation.cs
+++ MathGame/Assets/Scripts/PlayAnimation.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayAnimation : MonoBehaviour 
 {
 
 	private Animator ani;
-	public string stateName;
 
 	/// <summary>
 	/// Start this instance.
@@ -20,8 +20,26 @@ public class PlayAnimation : MonoBehaviour
 	/// Play animation associated with the GameObject
 	/// this script is attached to
 	/// </summary>
-	public void Animate() 
+	public void Animate(string state) 
+	{
+		ani.Play (state, -1, 0f);
+	}
+
+	public IEnumerator AnimateAndWait(string stateName) 
 	{
 		ani.Play (stateName, -1, 0f);
+
+		float length = 0;
+		AnimationClip[] clips = ani.runtimeAnimatorController.animationClips;
+
+		var clip = clips.Where ((AnimationClip arg) => (arg.name == stateName)).SingleOrDefault();
+		if (clip != null)
+		{
+			length = clip.length;
+		}
+
+		//var length = ani.GetCurrentAnimatorClipInfo (0).LongLength;
+		Debug.Log (length);
+		yield return new WaitForSeconds (length);
 	}
 }
---
+++ MathGame/Assets/Scripts/SubtractionProblem.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using AssemblyCSharp;
+
+
+public class SubtractionProblem : MonoBehaviour {
+
+	#region Variable Declararion
+	public Text mathProblem, userInput;
+	public Button enterButton;
+
+	public Text score;
+	private MathEquation equation;
+
+	private const string INPUT = "userInput";
+	private const string CHEST_OPEN = "ChestAnimation";
+	private const string CHEST_LOCKED = "ChestLocked";
+	private const string COIN_EARNED = "CoinAnimation";
+
+	private GameObject inputFieldGO;
+	private InputField InputFieldCO;
+	private bool isFocused;
+
+	private int correctAnswers;
+	private int level;
+	private int increaseRange;
+
+	public GameObject coinGO;
+	private PlayAnimation coinScript;
+	public GameObject chestGO;
+	private PlayAnimation chestScript;
+	#endregion
+
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	void Start ()
+	{
+		// get access to script on Chest and Coin object to play it's animation
+		coinScript = coinGO.GetComponent<PlayAnimation> ();
+		chestScript = chestGO.GetComponent<PlayAnimation> ();
+
+		// add a listener for when user clicks enter button
+		enterButton
+			.onClick
+			.AddListener (CheckAnswer);
+
+		// get the input field as a game object and an input field object
+		inputFieldGO = GameObject.Find (INPUT);
+		InputFieldCO = inputFieldGO.GetComponent<InputField> ();
+
+		isFocused = false;
+
+		correctAnswers = 0;
+		level = 1;
+
+		score.text = correctAnswers.ToString();
+		increaseRange = 10;
+
+		// get the first math equation and set the text
+		equation = new MathEquation (increaseRange, level, MathEquation.EquationType.Subtraction);
+		mathProblem.text = equation.Num1 + " - " + equation.Num2 + " = ";
+	}
+
+	/// <summary>
+	/// Update is called once per frame, checks user input
+	/// </summary>
+	void Update () 
+	{
+		inputFieldGO = GameObject.Find (INPUT);
+
+		// if the user presses enter, take that as if they clicked the enter button
+		// check if the answer is correct
+		if (Input.GetKeyDown (KeyCode.Return) && isFocused)
+		{
+			CheckAnswer ();
+		} 
+		else 
+		{
+			// update the number being displayed in the input box to reflect
+			// what the user has entered into it
+			userInput.text = InputFieldCO.text;
+		}
+
+		// keep tabs of if input box is in focus or not
+		// needed to register if input box is in focus and user clicks enter
+		// because when the user hits enter it immediately goes out of focus
+		if (InputFieldCO.isFocused)
+		{
+			isFocused = true;
+		}
+		else
+		{
+			isFocused = false;
+		}
+	}
+
+	/// <summary>
+	/// Checks the answer to the math problem given by the user.
+	/// If correct, a new math problem is generated.
+	/// </summary>
+	void CheckAnswer() 
+	{
+
+		// get the number entered by the user.
+		// don't really need to validate it's a number because the input
+		// field only allows integers
+		int input;
+		int.TryParse(InputFieldCO.text, out input);
+
+		if (input == equation.Difference) 
+		{ // user answered correctly
+
+			// play animation of treasure chest opening & coin going into scor
+			chestScript.Animate(CHEST_OPEN);
+			// play animation of coin going into score - wait until animation finishes
+			StartCoroutine(coinScript.AnimateAndWait (COIN_EARNED));
+
+
+
+			// generate a new math problem & update display
+			correctAnswers++;
+			equation.GenerateNewEquation ();
+			mathProblem.text = equation.Num1 + " - " + equation.Num2 + " = ";
+			InputFieldCO.text = string.Empty;
+			InputFieldCO.ActivateInputField();
+
+			if (correctAnswers % 10 == 0)
+			{
+				equation.IncreaseLevel ();
+			}
+
+			score.text = correctAnswers.ToString();
+		} 
+		else 
+		{ // user answered incorrectly
+			InputFieldCO.ActivateInputField();
+			chestScript.Animate (CHEST_LOCKED);
+		}
+	}
+}
