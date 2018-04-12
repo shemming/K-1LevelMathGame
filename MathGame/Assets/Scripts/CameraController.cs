8f2323af853c0ddd4298bb61fd2eb661b853a3f7
Sabrina <shemming444@gmail.com>
2018-04-12 19:32:03 -0400

    Main Area Design
    - design main area.
    
    - create a border to main area.
    
    - create player that moves with animation.
    
    - add colliders to prevent player from leaving area.
    
    CameraController.cs - have camera follow the player across the map without displaying outside of the map.
    
    PlayerController.cs - Have player move across map with keypresses, have run animation play when moving and face the direction its moving in.

---
+++ MathGame/Assets/Scripts/CameraController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	//Public variable to store a reference to the player game object
+	public GameObject player;   
+
+	// Public variable to store a reference to the camera following the game object
+	public Camera myCamera;
+
+	//Private variable to store the offset distance between the player and camera
+	private Vector3 offset;         
+
+	// The size of the map in terms of x and y axis
+	float mapX = 17.92f;
+	float mapY = 10.24f;
+
+	// Stores the min and max X and Y coordinates for the camera
+	private float minX;
+	private float maxX;
+	private float minY;
+	private float maxY;
+
+	// Use this for initialization
+	void Start () 
+	{
+		var vertExtent = myCamera.orthographicSize;
+		var horzExtent = vertExtent * Screen.width / Screen.height;
+
+		// Calculations assume map is position at the origin
+		minX = horzExtent - mapX / 2.0f;
+		maxX = mapX / 2.0f - horzExtent;
+		minY = vertExtent - mapY / 2.0f;
+		maxY = mapY / 2.0f - vertExtent;
+
+		//Calculate and store the offset value by getting the distance between the player's position and camera's position.
+		offset = transform.position - player.transform.position;
+	}
+
+	// LateUpdate is called after Update each frame
+	void LateUpdate () 
+	{
+		
+		// Set the position of the camera's transform to be the same as the player's, 
+		// but offset by the calculated offset distance.
+		transform.position = player.transform.position + offset;
+
+		// make sure the camera doesn't leave the map area
+		var v3 = transform.position;
+		v3.x = Mathf.Clamp(v3.x, minX, maxX);
+		v3.y = Mathf.Clamp(v3.y, minY, maxY);
+		transform.position = v3;
+	}
+}
---
+++ MathGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	//Floating point variable to store the player's movement speed.
+	public float speed;             
+
+	//Store a reference to the Rigidbody2D component required to use 2D Physics.
+	private Rigidbody2D rb2d;       
+	// Store a reference to the animator controller to animate movement
+	private Animator ani;
+	// Holds if the character was last moving right or left
+	private bool left, right;
+
+	// Use this for initialization
+	void Start()
+	{
+		//Get and store a reference to the Rigidbody2D component 
+		rb2d = GetComponent<Rigidbody2D> ();
+
+		// Get and store a reference to the animator controller
+		ani = GetComponent<Animator> ();
+
+		left = false;
+		right = false;
+	}
+
+	void Update() 
+	{
+		// set bool to know the last left or right arrow clicked if going up or down
+		if (Input.GetKeyDown (KeyCode.LeftArrow))
+		{
+			left = true;
+			right = false;
+		}
+		else if (Input.GetKeyDown (KeyCode.RightArrow))
+		{
+			right = true;
+			left = false;
+		}
+
+		// choose which direction to face for animation and play it
+		// if left arrow is being clicked or up/down arrow is being clicked and left arrow was clicked last
+		if (Input.GetKey (KeyCode.LeftArrow) ||
+			((Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.DownArrow)) && left))
+		{
+			rb2d.transform.eulerAngles = new Vector3(0, 180, 0);
+			ani.SetBool ("walkRight", true);
+		}
+		// if right arrow is being clicked or up/down arrow is being clicked and right arrow was clicked last
+		else if (Input.GetKey (KeyCode.RightArrow) ||
+			((Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.DownArrow)) && right))
+		{
+			rb2d.transform.eulerAngles = new Vector3(0, 0, 0);
+			ani.SetBool ("walkRight", true);
+		}
+		// character is not moving
+		else
+		{
+			ani.SetBool ("walkRight", false);
+		}
+	}
+
+	void FixedUpdate() 
+	{
+		//Store the current horizontal input in the float moveHorizontal.
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+		//Store the current vertical input in the float moveVertical.
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		//Use the two store floats to create a new Vector2 variable movement.
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+
+		//Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+		rb2d.AddForce (movement * speed);
+	}
+}
